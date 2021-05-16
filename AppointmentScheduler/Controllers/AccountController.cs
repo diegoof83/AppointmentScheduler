a300807d3264f66c4bf627033c9340a7fecf8c36
Diego Ferreira <diegoof@gmail.com>
2021-05-16 02:13:12 -0400

    Created -PartialView for Login navbar -Login ActionResult -Sign Out ActionResult -Application Identity functionality
    Added
    -asp-validation-summary
    notify.min.js
    script-custon-calendar

--- AppointmentScheduler/Controllers/AccountController.cs
+++ AppointmentScheduler/Controllers/AccountController.cs
@@ -1,4 +1,7 @@
 ﻿using AppointmentScheduler.Models;
+using AppointmentScheduler.Models.ViewModels;
+using AppointmentScheduler.Utility;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,10 +13,17 @@ namespace AppointmentScheduler.Controllers
     public class AccountController : Controller
     {
         private readonly ApplicationDbContext _dbContext;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public AccountController(ApplicationDbContext dbContext)
+        public AccountController(ApplicationDbContext dbContext, UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager, RoleManager<IdentityRole> roleManager)
         {
             _dbContext = dbContext;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
         }
 
         public IActionResult Login()
@@ -21,9 +31,69 @@ namespace AppointmentScheduler.Controllers
             return View();
         }
 
-        public IActionResult Register()
+        [HttpPost]
+        public async Task<IActionResult> Logoff()
         {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Login", "Account");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", "Invalid email or password.");
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> Register()
+        {
+            if (!_roleManager.RoleExistsAsync(Helper.Admin).GetAwaiter().GetResult())
+            {
+                await _roleManager.CreateAsync(new IdentityRole(Helper.Admin));
+                await _roleManager.CreateAsync(new IdentityRole(Helper.Doctor));
+                await _roleManager.CreateAsync(new IdentityRole(Helper.Patient));
+            }
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser
+                {
+                    UserName = model.Name,
+                    Email = model.Email
+                };
+
+                //create a new user
+                var result = await _userManager.CreateAsync(user, model.Password);
+                //sign in the new user
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(user, model.RoleName);
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+                //show message error in case they happen
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View(model);
         }
     }
 }
